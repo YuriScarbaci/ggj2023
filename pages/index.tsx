d9888dc9455957039c99f83dc7dd20c6e90e9cae
yuri.scarbaci <yuri.scarbaci@leniolabs.com>
2023-02-04 18:30:02 +0100

    chore: index place-holder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -15,8 +16,36 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr",
+            gridTemplateRows: "1fr 1fr",
+            gap: "0px 0px",
+            justifyContent: "center",
+            alignContent: "center",
+            justifyItems: "center",
+            alignItems: "center",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <h2> Imagine some really neat graphic and presentation here!</h2>
+
+          <Link
+            href="/play"
+            style={{
+              padding: "10px",
+              background: "green",
+              fontSize: "35px",
+              color: "white",
+              borderRadius: "30%",
+            }}
+          >
+            Play
+          </Link>
+        </div>
       </main>
     </>
-  )
+  );
 }
