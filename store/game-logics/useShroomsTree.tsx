93c5ac4d722fc6eea11ed6c27f595dc21ee3da90
yuri.scarbaci <yuri.scarbaci@leniolabs.com>
2023-02-05 15:22:25 +0100

    chore: minor useShroomTree refactor

--- store/game-logics/useShroomsTree.tsx
+++ store/game-logics/useShroomsTree.tsx
@@ -1,9 +1,69 @@
 import * as React from "react";
 import { v4 as uuid } from "uuid";
 import { AnchorPoint, ColonyPoint, TerritoryType } from "@/store/types";
+import { useTraitPoints } from "@/store/game-logics/useTraitPoints";
 import TreeModel from "tree-model";
 
-export const useShroomsTree = (config) => {
+type Config = {
+  setTotalColonies: React.Dispatch<React.SetStateAction<number>>;
+  addTraitPoints: ReturnType<typeof useTraitPoints>["addTraitPoints"];
+};
+
+const canTheRootExpand = ({
+  anchorPoint,
+  parentNode,
+  expandCost,
+}: {
+  anchorPoint: AnchorPoint;
+  parentNode: TreeModel.Node<ColonyPoint>;
+  expandCost: number;
+}) => {
+  if (anchorPoint.territoryType === "desert") return false;
+  const parentMinusCost = parentNode.model.rootPoints - expandCost;
+  if (parentMinusCost < 0) {
+    return false;
+  }
+
+  return true;
+};
+const handleScores = ({
+  anchorPoint,
+  addTraitPoints,
+  setTotalColonies,
+}: {
+  anchorPoint: AnchorPoint;
+  addTraitPoints: Config["addTraitPoints"];
+  setTotalColonies: Config["setTotalColonies"];
+}) => {
+  setTotalColonies((o: number) => o + 1);
+  if (anchorPoint.territoryType === "colonyPoint") addTraitPoints(1);
+};
+const handleNodesRootPointsAndChildren = ({
+  anchorPoint,
+  parentNode,
+  expandCost,
+  fungiTree,
+}: {
+  anchorPoint: AnchorPoint;
+  parentNode: TreeModel.Node<ColonyPoint>;
+  expandCost: number;
+  fungiTree: TreeModel;
+}) => {
+  const parentMinusCost = parentNode.model.rootPoints - expandCost;
+  const newRootPoints = Math.floor(parentMinusCost / 2);
+  const parentMinusCostMinusShare = parentMinusCost - newRootPoints;
+  const newNode = fungiTree.parse({
+    ...anchorPoint,
+    fungusType: "colony",
+    rootPoints: newRootPoints,
+    hitPoints: 5,
+  });
+  parentNode.model.rootPoints = parentMinusCostMinusShare;
+  parentNode.addChild(newNode);
+};
+
+export const useShroomsTree = (config: Config) => {
+  const { setTotalColonies, addTraitPoints } = config;
   const [treeRerenderKey, setTreeRerenderKey] = React.useState(0);
   const fungiTree = React.useMemo(() => new TreeModel(), []);
   const [rootNode, setRootNode] = React.useState(
@@ -30,25 +90,20 @@ export const useShroomsTree = (config) => {
     }) => {
       const [t1, t2] = [parentNode.model.t, anchorPoint.t].sort();
       const expandCost = t2 - t1;
-      const parentMinusCost = parentNode.model.rootPoints - expandCost;
-      if (parentMinusCost < 0) {
+
+      if (canTheRootExpand({ expandCost, anchorPoint, parentNode }) === false)
         return null;
-      }
-      const newRootPoints = Math.floor(parentMinusCost / 2);
-      const parentMinusCostMinusShare = parentMinusCost - newRootPoints;
-      const newNode = fungiTree.parse({
-        ...anchorPoint,
-        fungusType: "colony",
-        rootPoints: newRootPoints,
-        hitPoints: 5,
+
+      handleNodesRootPointsAndChildren({
+        anchorPoint,
+        parentNode,
+        expandCost,
+        fungiTree,
       });
-      parentNode.addChild(newNode);
-      parentNode.model.rootPoints = parentMinusCostMinusShare;
-      config.setTotalColonies((o: number) => o + 1);
+      handleScores({ anchorPoint, setTotalColonies, addTraitPoints });
       setTreeRerenderKey((o) => o + 1);
-      if (anchorPoint.territoryType === "colonyPoint") config.addTraitPoints(1);
     },
-    [fungiTree, config]
+    [fungiTree, setTotalColonies, addTraitPoints]
   );
   return { addRoot, treeRerenderKey, setTreeRerenderKey, fungiTree, rootNode };
 };
