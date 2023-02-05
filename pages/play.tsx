2caf7cfdb8daf53653344f6b42a3eca856b631ba
Ezequiel Baruf <ezequiel.baruf@leniolabs.com>
2023-02-05 15:20:43 -0300

    added credits and name image

--- pages/play.tsx
+++ pages/play.tsx
@@ -1,5 +1,4 @@
-// import Head from "next/head";
-import Image from "next/image";
+import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { EventsStore, GameStore } from "@/store";
 import { GameCanvas } from "../components";
@@ -15,24 +14,32 @@ import { Enemies } from "@/components/instancied/Enemies";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <EventsStore>
-        <GameStore>
-          <GameCanvas>
-            <Background />
-            <FungiInventoryPanel />
-            <BoardTraits />
-            <BoardGlobalStats />
-            <GameCamera>
-              <Soil>
-                <AnchorPoints />
-                <FungiColonies />
-                <Enemies />
-              </Soil>
-            </GameCamera>
-          </GameCanvas>
-        </GameStore>
-      </EventsStore>
-    </main>
+    <>
+      <Head>
+        <title>Mushroomize</title>
+        <meta name="description" content="Mushroomize play page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <EventsStore>
+          <GameStore>
+            <GameCanvas>
+              <Background />
+              <FungiInventoryPanel />
+              <BoardTraits />
+              <BoardGlobalStats />
+              <GameCamera>
+                <Soil>
+                  <AnchorPoints />
+                  <FungiColonies />
+                  <Enemies />
+                </Soil>
+              </GameCamera>
+            </GameCanvas>
+          </GameStore>
+        </EventsStore>
+      </main>
+    </>
   );
 }
